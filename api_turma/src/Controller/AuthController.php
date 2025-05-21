7d14dd2ebd48f5e5c53fd6830c109102671b6046
AdrianoEusebio <adrianoeusebio2006c1@email.com>
2025-05-21 15:42:10 -0400

    feat: criação dos endpoints que cria e authentica o admin e professor

---
+++ api_turma/src/Controller/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Administrador;
+use App\Entity\Professor;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AuthController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $em)
+    {}
+    
+    #[Route('/login',methods:['POST'])]
+    public function login(Request $request): JsonResponse
+    {
+        $data = json_encode($request->getContent(), true);
+
+        $email = $data['email'] ?? null;
+        $senha = $data['senha'] ?? null;
+
+        if(!$email || !$senha){
+            return new JsonResponse(['erro' => 'Email e senha são obrigatorios'],400);
+        }
+
+        $admin = $this->em->getRepository(Administrador::class)
+            ->findOneBy(['email' => $email]);
+        
+        if(@$admin && password_hash($senha, $admin->getSenhaHash())){
+            return new JsonResponse([
+                'message' => 'Login Adminstrado bem-sucedido',
+                'role' => 'admin',
+                'id' => $admin->getId()
+            ]);
+        }
+
+        $professor = $this->em->getRepository(Professor::class)
+            ->findOneBy(['email' => $email]);
+
+        if ($professor && password_verify($senha, $professor->getSenhaHash())) {
+            return new JsonResponse([
+                'message' => 'Login Professor bem-sucedido',
+                'role' => 'professor',
+                'id' => $professor->getId()
+            ]);
+        }
+
+        return new JsonResponse(['error' => 'Credenciais inválidas'], 401);
+    }
+}
---
+++ api_turma/src/Controller/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Administrador;
+use App\Entity\Professor;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class UserController extends AbstractController
+{
+    public function __construct(private EntityManagerInterface $em) {}
+    
+    #[Route('/criar-admin', methods: ['POST'])]
+    public function criarAdmin(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $admin = new Administrador();
+        $admin->setNome($data['nome']);
+        $admin->setEmail($data['email']);
+        $admin->setSenhaHash(password_hash($data['senha'], PASSWORD_DEFAULT));
+
+        $this->em->persist($admin);
+        $this->em->flush();
+
+        return new JsonResponse(['message' => 'Administrador criado com sucesso', 'id' => $admin->getId()]);
+    }
+
+    #[Route('/criar-professor', methods: ['POST'])]
+    public function criarProfessor(Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $professor = new Professor();
+        $professor->setNome($data['nome']);
+        $professor->setEmail($data['email']);
+        $professor->setSenhaHash(password_hash($data['senha'], PASSWORD_DEFAULT));
+
+        $this->em->persist($professor);
+        $this->em->flush();
+
+        return new JsonResponse(['message' => 'Professor criado com sucesso', 'id' => $professor->getId()]);
+    }
+}
